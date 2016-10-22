6daf2e5d9a42c90910337854e46a667da998f7a7
bradmbaker <bradmbak@gmail.com>
2016-10-22 10:41:30 -0400

    adding sleep

--- js/main-1.6.js
+++ js/main-1.6.js
@@ -92,5 +92,9 @@ $(function() {
     $('figure.responsive-image').picture();
 
     // Makes screen size correct
-    $("#secret-section").height($("#secret-section-title").height() + $("#secret-section-pics").height() + 100)
+	setTimeout(function(){
+	    $("#secret-section").height($("#secret-section-title").height() + $("#secret-section-pics").height() + 100)
+	}, 1000);
+
+    
 });
